287554dd367f7ce2f179a089a3d5058a2f3dbab2
Swizec Teller <swizec@swizec.com>
2025-12-26 17:24:18 +0100

    Playwright works

--- index.ts
+++ index.ts
@@ -1,9 +1,25 @@
 import OpenAI from "openai";
-const client = new OpenAI();
+import { chromium } from "playwright";
 
-const response = await client.responses.create({
-    model: "gpt-5",
-    input: "Write a short poem about TypeScript.",
+const browser = await chromium.launch({
+    headless: false,
+    chromiumSandbox: true,
+    env: {},
+    args: ["--disable-extensions", "--disable-filesystem"],
 });
 
-console.log(response.output_text);
+const page = await browser.newPage();
+await page.setViewportSize({ width: 1280, height: 720 });
+await page.goto("https://scalingfastbook.com");
+await page.waitForTimeout(10000);
+
+browser.close();
+
+// const client = new OpenAI();
+
+// const response = await client.responses.create({
+//     model: "gpt-5-nano",
+//     input: "Write a super short bed time story about a unicorn who learns to code in JavaScript.",
+// });
+
+// console.log(response.output_text);
