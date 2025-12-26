1db52f548fbcc3385cfcba167240c2d35b536368
Swizec Teller <swizec@swizec.com>
2025-12-26 17:40:52 +0100

    Sending initial screenshot with first call doesn't work

--- index.ts
+++ index.ts
@@ -1,6 +1,8 @@
 import OpenAI from "openai";
 import { chromium } from "playwright";
 
+const openai = new OpenAI();
+
 const browser = await chromium.launch({
     headless: false,
     chromiumSandbox: true,
@@ -11,9 +13,47 @@ const browser = await chromium.launch({
 const page = await browser.newPage();
 await page.setViewportSize({ width: 1280, height: 720 });
 await page.goto("https://scalingfastbook.com");
-await page.waitForTimeout(10000);
 
-browser.close();
+const screenshotBuffer = await page.screenshot({ type: "png" });
+
+const response = await openai.responses.create({
+    model: "computer-use-preview",
+    tools: [
+        {
+            type: "computer_use_preview",
+            display_width: 1280,
+            display_height: 720,
+            environment: "browser",
+        },
+    ],
+    input: [
+        {
+            role: "user",
+            content:
+                "Sign up for a preview of the Scaling Fast book at scalingfastbook.com",
+        },
+    ],
+    reasoning: { summary: "concise" },
+    truncation: "auto",
+});
+
+await browser.close();
+
+console.log(response.output);
+
+// const browser = await chromium.launch({
+//     headless: false,
+//     chromiumSandbox: true,
+//     env: {},
+//     args: ["--disable-extensions", "--disable-filesystem"],
+// });
+
+// const page = await browser.newPage();
+// await page.setViewportSize({ width: 1280, height: 720 });
+// await page.goto("https://scalingfastbook.com");
+// await page.waitForTimeout(10000);
+
+// browser.close();
 
 // const client = new OpenAI();
 
